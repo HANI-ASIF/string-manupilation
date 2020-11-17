1d1125a38b958bf5f5df78ef41bfdf3afc24990c
HANI-ASIF <73904132+HANI-ASIF@users.noreply.github.com>
2020-11-16 22:32:15 -0800

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim N As Integer = 0
+        Dim sum As Integer = 0
+        Dim x As Integer = 0
+        Dim y As Integer = 1
+        Console.Write("enter febonacci range: ")
+        N = Console.ReadLine()
+        Console.Write(Str(x) & ", " & Str(y) & ", ")
+        While sum <= N
+            sum = x + y
+
+            Console.Write(Str(sum) & ", ")
+            x = y
+            y = sum
+
+        End While
+        Console.ReadKey()
+    End Sub
+
+End Module
